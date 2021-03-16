af875d9c5a37af07acef1329cd9a194c266feb1a
pj.chen <chen.pj@inventec.com>
2021-03-16 09:57:33 +0800

    [inventec][transformers] - Move gpio essential init setting to init script
    Symtom/Reason:
        Add initial gpio setting required by CPLD to transformers-init.service.
    
    Root Cause:
        CPLD needs BMC's state to get to next stage.
    
    Solution/Change:
        [transformers-dts]
            Change the gpio pin define for DVT.
        [transformers-init]
            Set gpio pin value.
            And remove udhcpc part which is no need.
        [bmc-ready]
            Since the gpio setting is set in init service.
            We have no need to using bmc-ready package.
    
    Entry Test:
        The system can be boot in to BIOS.
        If the gpio not set, CPLD would stop at initial state.

--- meta-inventec/meta-transformers/recipes-phosphor/bmc-ready/bmc-ready/bmc-ready.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-gpioutil -p b1 -d out -v 0
-echo BMC ready !!
-gpioutil -p f4 -d out -v 1
-echo Release reset SGPIO !!
--- meta-inventec/meta-transformers/recipes-phosphor/init/transformers-init/transformers-init.sh
+++ meta-inventec/meta-transformers/recipes-phosphor/init/transformers-init/transformers-init.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
-# Work around for systemd-networkd.service fail
-echo "Sleep 60s to wait systemd-networkd.service fail"
-sleep 60
-echo "Do udhcpc"
-/sbin/udhcpc eth0
+# Init GPIO setting
+
+gpioset `gpiofind BMC_READY`=0
+echo BMC ready !!
+gpioset `gpiofind RST_BMC_SGPIO`=1
+echo Release reset SGPIO !!
+gpioset `gpiofind RESET_OUT`=1
+echo bmc notify cpld to reset system, set high !!
+gpioset `gpiofind POWER_OUT`=1
+echo bmc notify cpld to power on system, set high !!
