1ac1cef0a0f8d13a28377f80955a2fecf757bcd7
emil <huang.emil@inventec.com>
2021-08-06 13:34:03 +0800

    [transformers-nuv]: Move fan control function from transformers to here and remove the fans function of nuvoton.

---
+++ meta-inventec/meta-transformers-nuv/recipes-phosphor/fan-ctl/phosphor-pid-control/fan-default-speed.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+HWMON_MAX=15
+FAN_MAX=7
+
+for num in $(seq 0 $HWMON_MAX) ; do
+
+        name=`cat /sys/class/hwmon/hwmon$num/name`
+        if [ $name == "npcm7xx_pwm_fan" ]; then
+            echo "fan driver found"
+			
+			for i in $(seq 0 $FAN_MAX) ; do
+				echo 255 > /sys/class/hwmon/hwmon$num/pwm$i
+			done
+           exit 0
+        fi
+
+done
+echo "$name is not a fan driver"
+exit 1
+
