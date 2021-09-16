299627aa87b5401d9cf775a3b63a69a14bac466e
link <link@inventec.com>
2021-09-16 11:39:53 +0800

    Bug 541 - [SW][transformers-nuv][cpld update] - Add cpld update from transformers     Symptom/Reason :         - Add cpld-update from transformers for supporting nuvton     Root Cause     : N/A
        Solution/Change:
            [cpld-update]
                - Adding cpld update from transformers
        Entry Test     :
            <check cpld-image exist>
                sysadmin@transformers-nuv:/tmp# ls
                Transformer_SCM_cpld_dvt_20210308-2_2_a.hex
            <cpld update>
                sysadmin@transformers-nuv:/tmp# cpldupdate-i2c Transformer_SCM_cpld_dvt_20210308-2_2_a.hex
                SCM_CPLD_TYPE
                Enable Program - Transparent Mode
                pre verify check pass
                Erase Flash CFG only
                - Program CFG Page -
                  (35648/35648) (100%/100%)     ...Done!
                - Verify CFG Page -
                ...Done!/35648) (100%/100%)
                Program Done
                program done succeed
                read status succeed
                cpld update done, ready to refresh...

---
+++ meta-inventec/meta-transformers-nuv/recipes-phosphor/cpldupdate/files/cpld_info.hpp
@@ -0,0 +1,6 @@
+const unsigned long SCM_CPLD_I2C_BUS =2;
+const unsigned long MB_CPLD_I2C_BUS =2;
+/*SCM*/
+const unsigned long SCM_CPLD_I2C_ADDR = 0x40;
+/*MB*/
+const unsigned long MB_CPLD_I2C_ADDR = 0x2c;
