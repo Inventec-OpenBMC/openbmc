c44b37c92fe8131b1c60730b9e98935256a2a9fc
pj.chen <chen.pj@inventec.com>
2021-10-19 14:08:51 +0800

    Bug 596 - [SW][Starscream][starscream-init] - Add i2c init
    Symptom/Reason:
        For riser1, 2, and back panel, not always plug on the device.
        In this case, define in dts would be hard to maintain.
        Change to use init script to init.
    
    Root Cause:
        N/A
    
    Solution/Change:
        [starscream-init]
            Add i2c probe commands.
    
    Entry Test:
    sysadmin@starscream:/sys/bus/i2c/devices/i2c-22# cat 22-0012/name
    pac1934
    sysadmin@starscream:/sys/bus/i2c/devices/i2c-22# cat 22-0012/hwmon/hwmon12/shunt_value
    4000 4000 4000 4000
    sysadmin@starscream:/sys/bus/i2c/devices/i2c-22# cat 22-0071/name
    pca9546
    sysadmin@starscream:/sys/bus/i2c/devices/i2c-22# cat i2c-30/30-0050/name
    24c32
    sysadmin@starscream:/sys/bus/i2c/devices/i2c-22# cat i2c-31/31-005c/name
    emc1403

--- meta-inventec/meta-starscream/recipes-phosphor/init/starscream-init/starscream-init.sh
+++ meta-inventec/meta-starscream/recipes-phosphor/init/starscream-init/starscream-init.sh
@@ -1,8 +1,110 @@
 #!/bin/sh
 
 # Init GPIO setting
-
 gpioset `gpiofind BMC_READY`=0
 echo BMC ready !!
 gpioset `gpiofind RST_BMC_SGPIO`=1
 echo Release reset SGPIO !!
+
+I2C_OFFSET=0
+CURRENT_I2C=27
+
+RISER2_MUX_I2C=21
+RISER2_MUX_I2C_CH0=$(($CURRENT_I2C+1))
+RISER2_MUX_I2C_CH1=$(($CURRENT_I2C+2))
+RISER2_MUX_I2C_CH2=$(($CURRENT_I2C+3))
+RISER2_MUX_I2C_CH3=$(($CURRENT_I2C+4))
+echo pca9546 0x71 > /sys/bus/i2c/devices/i2c-$RISER2_MUX_I2C/new_device
+
+if [ -d "/sys/bus/i2c/devices/i2c-$RISER2_MUX_I2C_CH0" ]
+then
+    RISER2_PRESENT="true"
+    # PAC1934 U1
+    echo pac1934 0x12 > /sys/bus/i2c/devices/i2c-$RISER2_MUX_I2C/new_device
+    shunt_path=$(ls /sys/bus/i2c/devices/i2c-$RISER2_MUX_I2C/$RISER2_MUX_I2C-0012/hwmon/hwmon*/shunt_value)
+    # shunt is 0.004 ohm
+    echo 0 4000 > $shunt_path
+    echo 1 4000 > $shunt_path
+    echo 2 4000 > $shunt_path
+    echo 3 4000 > $shunt_path
+
+    echo 24c32 0x50 > /sys/bus/i2c/devices/i2c-$RISER2_MUX_I2C_CH2/new_device
+    echo emc1403 0x5c > /sys/bus/i2c/devices/i2c-$RISER2_MUX_I2C_CH3/new_device
+    CURRENT_I2C=$(($CURRENT_I2C+4))
+else
+    RISER2_PRESENT="false"
+fi
+
+RISER1_MUX_I2C=22
+RISER1_MUX_I2C_CH0=$(($CURRENT_I2C+1))
+RISER1_MUX_I2C_CH1=$(($CURRENT_I2C+2))
+RISER1_MUX_I2C_CH2=$(($CURRENT_I2C+3))
+RISER1_MUX_I2C_CH3=$(($CURRENT_I2C+4))
+echo pca9546 0x71 > /sys/bus/i2c/devices/i2c-$RISER1_MUX_I2C/new_device
+
+if [ -d "/sys/bus/i2c/devices/i2c-$RISER1_MUX_I2C_CH0" ]
+then
+    RISER1_PRESENT="true"
+
+    # PAC1934 U1
+    echo pac1934 0x12 > /sys/bus/i2c/devices/i2c-$RISER1_MUX_I2C/new_device
+    shunt_path=$(ls /sys/bus/i2c/devices/i2c-$RISER1_MUX_I2C/$RISER1_MUX_I2C-0012/hwmon/hwmon*/shunt_value)
+    # shunt is 0.004 ohm
+    echo 0 4000 > $shunt_path
+    echo 1 4000 > $shunt_path
+    echo 2 4000 > $shunt_path
+    echo 3 4000 > $shunt_path
+
+    echo 24c32 0x50 > /sys/bus/i2c/devices/i2c-$RISER1_MUX_I2C_CH2/new_device
+    echo emc1403 0x5c > /sys/bus/i2c/devices/i2c-$RISER1_MUX_I2C_CH3/new_device
+    CURRENT_I2C=$(($CURRENT_I2C+4))
+else
+    RISER1_PRESENT="false"
+fi
+
+
+echo "Riser1 $RISER1_PRESENT, Riser2 $RISER2_PRESENT"
+
+
+echo "Init BP part"
+
+BP1_MUX_U13_I2C=24
+BP1_MUX_U13_I2C_CH0=$(($CURRENT_I2C+1))
+BP1_MUX_U13_I2C_CH1=$(($CURRENT_I2C+2))
+BP1_MUX_U13_I2C_CH2=$(($CURRENT_I2C+3))
+BP1_MUX_U13_I2C_CH3=$(($CURRENT_I2C+4))
+echo pca9545 0x71 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C/new_device
+if [ -d "/sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C_CH0" ]
+then
+    BP1_PRESENT="true"
+    echo 24c32 0x50 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C/new_device
+    echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C_CH0/new_device
+    echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C_CH1/new_device
+    echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C_CH2/new_device
+    CURRENT_I2C=$(($CURRENT_I2C+16))
+else
+    BP1_PRESENT="false"
+fi
+
+BP2_MUX_U13_I2C=25
+BP2_MUX_U13_I2C_CH0=$(($CURRENT_I2C+1))
+BP2_MUX_U13_I2C_CH1=$(($CURRENT_I2C+2))
+BP2_MUX_U13_I2C_CH2=$(($CURRENT_I2C+3))
+BP2_MUX_U13_I2C_CH3=$(($CURRENT_I2C+4))
+echo pca9545 0x71 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C/new_device
+if [ -d "/sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C_CH0" ]
+then
+    BP2_PRESENT="true"
+    echo 24c32 0x50 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C/new_device
+    echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C_CH0/new_device
+    echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C_CH1/new_device
+    echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C_CH2/new_device
+    CURRENT_I2C=$(($CURRENT_I2C+4))
+else
+    BP2_PRESENT="false"
+fi
+
+
+echo "BP1 $BP1_PRESENT, BP2 $BP2_PRESENT"
+
+
