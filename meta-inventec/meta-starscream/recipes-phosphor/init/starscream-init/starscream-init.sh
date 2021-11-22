b8fbde107c369de5d7482a183fa243e78f8e5ab8
pj.chen <chen.pj@inventec.com>
2021-11-22 10:08:51 +0800

    BringUp - [Starscream][init] - Add power monitor config for BP
    Symptom/Reason:
        Add shunt value and modify driver name for BP.
    
    Root Cause:
        N/A
    
    Solution/Change:
        [starscream-init]
            Add shunt value and modify i2c mux driver for BP.
    
    Entry Test:
        N/A

--- meta-inventec/meta-starscream/recipes-phosphor/init/starscream-init/starscream-init.sh
+++ meta-inventec/meta-starscream/recipes-phosphor/init/starscream-init/starscream-init.sh
@@ -14,7 +14,7 @@ RISER1_MUX_I2C_CH0=$(($CURRENT_I2C+1))
 RISER1_MUX_I2C_CH1=$(($CURRENT_I2C+2))
 RISER1_MUX_I2C_CH2=$(($CURRENT_I2C+3))
 RISER1_MUX_I2C_CH3=$(($CURRENT_I2C+4))
-echo pca9546 0x71 > /sys/bus/i2c/devices/i2c-$RISER1_MUX_I2C/new_device
+echo pca9545 0x71 > /sys/bus/i2c/devices/i2c-$RISER1_MUX_I2C/new_device
 
 if [ -d "/sys/bus/i2c/devices/i2c-$RISER1_MUX_I2C_CH0" ]
 then
@@ -40,7 +40,7 @@ RISER2_MUX_I2C_CH0=$(($CURRENT_I2C+1))
 RISER2_MUX_I2C_CH1=$(($CURRENT_I2C+2))
 RISER2_MUX_I2C_CH2=$(($CURRENT_I2C+3))
 RISER2_MUX_I2C_CH3=$(($CURRENT_I2C+4))
-echo pca9546 0x71 > /sys/bus/i2c/devices/i2c-$RISER2_MUX_I2C/new_device
+echo pca9545 0x71 > /sys/bus/i2c/devices/i2c-$RISER2_MUX_I2C/new_device
 
 if [ -d "/sys/bus/i2c/devices/i2c-$RISER2_MUX_I2C_CH0" ]
 then
@@ -78,6 +78,14 @@ if [ -d "/sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C_CH0" ]
 then
     BP1_PRESENT="true"
     echo 24c32 0x50 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C/new_device
+
+    # PAC1932 U7
+    echo pac1934 0x12 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C/new_device
+    shunt_path=$(ls /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C/$BP1_MUX_U13_I2C-0012/hwmon/hwmon*/shunt_value)
+    # shunt is 0.0005, 0.01 ohm
+    echo 0 500 > $shunt_path
+    echo 1 10000 > $shunt_path
+
     echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C_CH0/new_device
     echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C_CH1/new_device
     echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP1_MUX_U13_I2C_CH2/new_device
@@ -96,6 +104,14 @@ if [ -d "/sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C_CH0" ]
 then
     BP2_PRESENT="true"
     echo 24c32 0x50 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C/new_device
+
+    # PAC1932 U7
+    echo pac1934 0x12 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C/new_device
+    shunt_path=$(ls /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C/$BP2_MUX_U13_I2C-0012/hwmon/hwmon*/shunt_value)
+    # shunt is 0.0005, 0.01 ohm
+    echo 0 500 > $shunt_path
+    echo 1 10000 > $shunt_path
+
     echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C_CH0/new_device
     echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C_CH1/new_device
     echo pca9545 0x73 > /sys/bus/i2c/devices/i2c-$BP2_MUX_U13_I2C_CH2/new_device
